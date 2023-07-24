d74d66f4429ad8646780512961cad4d8c8e5afef
abshwabu <abdulazizredi90@gmail.com>
2023-07-24 15:44:37 +0300

    reWritten with match

--- todo.py
+++ todo.py
@@ -5,13 +5,18 @@ userInput = "Enter a task: "
 todoList = []
 
 while True:
-	task = input(userInput)
-	if task == './end':
-		break
-	else:
+	actions = input("what do you want todo? \nadd\nshow\nexit")
+	match actions:
+		case 'add':
+			task = input(userInput)
+			todoList.append(task)
+		case 'show':
+			for todo in todoList:
+				print(todo)
+		case 'exit':
+			break
 		
 		
-		todoList.append(task)
 		
-		for todo in todoList:
-			print(todo)
+		
+		
