b99764eab0ce6d05e4b29ae65070705726ce728d
abshwabu <abdulazizredi90@gmail.com>
2023-07-26 00:36:24 +0300

    number added

--- todo.py
+++ todo.py
@@ -5,18 +5,21 @@ userInput = "Enter a task: "
 todoList = []
 
 while True:
-	actions = input("what do you want todo? \nadd\nshow\nexit\n: ")
+	actions = input("what do you want todo? \nadd\nshow\nedit\ndone\nexit\n: ")
 	match actions:
 		case 'add':
 			task = input(userInput)
 			todoList.append(task)
 		case 'show':
 			for index, todo in enumerate(todoList):
-				print(index,todo)
+				print(index+1,todo)
 		case 'edit':
 			num = int(input('which one do you want to edit \nEnter number: '))
 			newTask = input('Enter the new task: ')
 			todoList[num-1]= newTask
+		case 'done':
+			num = int(input('which one do you finish \nEnter number: '))
+			todoList.pop(num-1)
 			
 		case 'exit':
 			break
