dd5ae30b9ba116c0833fff205848657cea1a2d9e
abshwabu <abdulazizredi90@gmail.com>
2023-07-26 00:12:31 +0300

    editted

--- todo.py
+++ todo.py
@@ -13,6 +13,11 @@ while True:
 		case 'show':
 			for todo in todoList:
 				print(todo)
+		case 'edit':
+			num = int(input('which one do you want to edit \nEnter number: '))
+			newTask = input('Enter the new task: ')
+			todoList[num-1]= newTask
+			
 		case 'exit':
 			break
 		
