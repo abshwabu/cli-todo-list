7141b35d6e56c59fc5dbbdbfde131c7cfa1e2246
abshwabu <abdulazizredi90@gmail.com>
2023-07-26 13:35:37 +0300

    editting from txt file

--- todo.py
+++ todo.py
@@ -32,7 +32,7 @@ while True:
 			for index, todo in enumerate(todoList):
 				print(index+1,todo)
 			num = int(input('which one do you want to edit \nEnter number: '))
-			newTask = input('Enter the new task: ')
+			newTask = input('Enter the new task: ') + '\n'
 			todoList[num-1]= newTask
 			file = open('list.txt','w')
 			file.writelines(todoList)
